3a93a87e00eb5442a4c9bb495ce801dcae620d05
Ricardo Perez Espinoza <rperezespinoza@outlook.com>
2020-06-16 16:19:41 -0500

    Se agregó un parámetro al clima

--- clima/clima.js
+++ clima/clima.js
@@ -2,7 +2,7 @@ const axios = require('axios').default;
 
 const getClima = async(lat, lon) => {
 
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2db54ca7c6c973e071e22ea781687311`);
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2db54ca7c6c973e071e22ea781687311&units=metric`);
 
     return resp.data.main.temp;
 
